f3c143793722cd1ebdbbe4481a23ff67c7537020
Muhammad Haikal Buldan <h.buldan246@gmail.com>
2023-04-12 15:00:06 +0700

    Tugas Praktikum 4

--- app/Models/Matkul.php
+++ app/Models/Matkul.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Matkul extends Model
 {
     use HasFactory;
+
+    protected $table = 'matkul';
+    protected $primaryKey = 'id';
+    protected $fillable = ['nama', 'kode', 'mata_kuliah', 'dosen', 'deskripsi_mata_kuliah', 'tanggal_kelas'];
 }
